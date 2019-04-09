42ed6c6bb763ef33d56b12ca8396017f4d39c2c5
Miles Marillo <mjm03@hampshire.edu>
2019-04-09 16:39:43 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def dictionary {
+  ""
+  
+  }
+end
+
+def word_substituter(tweets)
+end
+
+def bulk_tweet_shortener(tweets)
+end
+
+def selective_tweet_shortener(tweets)
+end
+
+def shortened_tweet_truncator(tweets)
+end
