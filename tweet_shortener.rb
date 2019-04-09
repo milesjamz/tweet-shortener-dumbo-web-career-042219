7e0f8e2796a3d4c2d092726531a891119b3c682f
Miles Marillo <mjm03@hampshire.edu>
2019-04-09 16:41:38 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,6 +14,15 @@ def dictionary {
 end
 
 def word_substituter(tweets)
+   tweet = tweets.split(" ")
+   tweet.each_with_index do |word, index|
+      dictionary.each do |key, value|
+         if word.downcase == key
+            tweet[index] = value
+         end
+      end
+   end
+   tweet.join(" ")
 end
 
 def bulk_tweet_shortener(tweets)
